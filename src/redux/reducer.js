4d371b5eed8a54f63a343ba26bc0359d0b344228
Awet <awetkifle94@gmail.com>
2020-08-12 01:05:09 +0100

    fix redux

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,24 +2,24 @@ import { TO_DO_LOAD, TO_DO_ADD, TO_DO_DELETE } from "./actions";
 
 const INITAL_TODOS = [];
 
-function bookingReducer(state = INITAL_TODOS, action) {
-    switch (action.type) {
-        case TO_DO_LOAD:
-            return [...state, action.initalToDos];
-        case TO_DO_ADD:
-            return [...state, action.toDo];
-        case TO_DO_DELETE:
-            return state.map((booking) => {
-                if (booking.id === action.bookingId) {
-                    return {
-                        ...booking,
-                        status: action.value,
-                    };
-                } else return booking;
-            });
-        default:
-            return state;
-    }
+function reducer(state = INITAL_TODOS, action) {
+  switch (action.type) {
+    case TO_DO_LOAD:
+      return [...state, action.initalToDos];
+    case TO_DO_ADD:
+      return [...state, action.toDo];
+    // case TO_DO_DELETE:
+    //     return state.map((toDo) => {
+    //         if (toDo.id === action.toDoId) {
+    //             return {
+    //                 ...booking,
+    //                 status: action.value,
+    //             };
+    //         } else return toDo;
+    //     });
+    default:
+      return state;
+  }
 }
 
-export default bookingReducer;
+export default reducer;
